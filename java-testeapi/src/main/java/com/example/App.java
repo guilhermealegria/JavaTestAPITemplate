ffe0f83a4917271d89b1938c7038c317488ae5ad
Guilherme Alegria <guilhermemoraes.3@gmail.com>
2022-12-07 16:13:30 -0300

    add new test cases

--- java-testeapi/src/main/java/com/example/App.java
+++ java-testeapi/src/main/java/com/example/App.java
@@ -1,13 +1,79 @@
 package com.example;
 
+import org.json.simple.JSONObject;
+
 /**
- * Hello world!
- *
+ * imports
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+
+public class App {
+
+    private String url;
+    private String cpfSemRestricao;
+    private String cpfcomRestricao;
+    private String cpfSimulacao;
+    private JSONObject usuario;
+    private JSONObject usuarioSemDados;
+    private String id;
+
+    public String getUrl(){
+        return url = "http://localhost:8080/api/v1/";
     }
+
+    
+    public JSONObject getUsuario(){
+        usuario = new JSONObject();
+        usuario.put("nome", "Guilherme");
+        usuario.put("cpf", 14145393716l);
+        usuario.put("email", "guilherme@gmail.com");
+        usuario.put("valor", 3200);
+        usuario.put("parcelas", 10);
+        usuario.put("seguro", true);
+        return usuario;
+    }
+
+    public JSONObject getEdicaoUsuario(){
+        this.usuario.put("nome", "Guilherme");
+        this.usuario.put("cpf", 14145393716l);
+        this.usuario.put("email", "guilherme@gmail.com");
+        this.usuario.put("valor", 4300);
+        this.usuario.put("parcelas", 5);
+        this.usuario.put("seguro", false);
+        return this.usuario;
+    }
+
+    public void setID (String id){
+        this.id = id;
+    }
+
+    public String getID(){
+        return this.id;
+    }
+
+    public String getcpfSemRestricao(){
+        return cpfSemRestricao = "03243021753";
+    }
+
+    public String getcpfComRestricao(){
+        return cpfcomRestricao = "97093236014";
+    }
+
+    public String getcpfSimulacao(){
+        return cpfSimulacao = "17822386034";
+    }
+
+
+    public JSONObject getUsuarioSemDados() {
+        usuarioSemDados = new JSONObject();
+        usuarioSemDados.put("nome", "Guilherme");
+        usuarioSemDados.put("email", "guilherme@gmail.com");
+        usuarioSemDados.put("valor", 3200);
+        usuarioSemDados.put("parcelas", 10);
+        usuarioSemDados.put("seguro", true);
+        return usuarioSemDados;
+    }
+
+
+    
+
 }
--- java-testeapi/src/test/java/com/example/AppTest.java
+++ java-testeapi/src/test/java/com/example/AppTest.java
@@ -1,34 +1,148 @@
 package com.example;
 
-import static org.junit.Assert.assertTrue;
-
-import org.junit.Test;
-
 /**
  * Import dependencies.
  */
-import org.json.JSONObject;
 import static io.restassured.RestAssured.given;
-import static io.restassured.RestAssured.when;
-import static io.restassured.RestAssured.get;
-import static org.hamcrest.core.IsEqual.equalTo;
-import static org.hamcrest.CoreMatchers.containsString; 
+import static org.hamcrest.CoreMatchers.containsString;
+import org.json.simple.JSONObject;
+import org.junit.Test;
+import org.junit.jupiter.api.Order;
+
+import io.restassured.RestAssured;
+import io.restassured.http.ContentType;
+import io.restassured.response.*;
+import io.restassured.specification.RequestSpecification;
 
 
-public class AppTest 
-{
+public class AppTest {
 
-    private String url = "https://jsonplaceholder.typicode.com/users/";
+    private App app = new App();
+    private JSONObject usuario  = app.getUsuario();
+    private JSONObject usuarioEdicao = app.getEdicaoUsuario();
+    private JSONObject usuarioSemDados = app.getUsuarioSemDados() ;
+    private Response response;
+    private RequestSpecification httpRequest = RestAssured.given();
+
     /**
      * Requisitions Test
      */
     @Test
-    public void getTest()
-    {
-        when()
-        .get(url)
+    @Order(1)
+    public void consultarCpfsemRestricao(){   
+        given()
+        .when()
+            .get(app.getUrl() + "restricoes/" + app.getcpfSemRestricao())
         .then()
-        .statusCode(200);
+            .statusCode(204);
 
     }
+
+    @Test
+    @Order(2)
+    public void consultarCpfcomRestricao(){
+        String mensagemRestricao = "O CPF " + app.getcpfComRestricao() + " tem problema";
+        given()
+        .when()
+            .get(app.getUrl() + "restricoes/" + app.getcpfComRestricao())
+        .then()
+            .statusCode(200)
+            .body(containsString(mensagemRestricao));
+    }
+
+    @Test
+    @Order(3)
+    public void consultarSimulacoes(){
+        given()
+        .when()
+            .get(app.getUrl() + "simulacoes")
+        .then()
+            .statusCode(200)
+            .body(containsString("id"));
+    }
+
+    @Test
+    @Order(4)
+    public void consultarSimulacaoPorCPF(){
+        given()
+        .when()
+            .get(app.getUrl() + "simulacoes/" + app.getcpfSimulacao())
+        .then()
+            .statusCode(200)
+            .body(containsString(app.getcpfSimulacao()));
+    }
+
+    @Test
+    @Order(5)
+    public void enviarSimulacao(){
+        given()
+            .body(usuario.toJSONString())
+            .contentType(ContentType.JSON)
+        .when()
+            .post(app.getUrl() + "simulacoes")
+        .then()
+            .statusCode(201)
+            .body(containsString(usuario.get("cpf").toString()));
+        
+
+    }
+
+    @Test
+    @Order(6)
+    public void enviarSimulacaoDuplicada(){
+        given()
+            .body(usuario.toJSONString())
+            .contentType(ContentType.JSON)
+        .when()
+            .post(app.getUrl() + "simulacoes")
+        .then()
+            .statusCode(400)
+            .body(containsString("CPF duplicado"));
+    }
+
+    @Test
+    @Order(7)
+    public void enviarSimulacaoComDadosemFalta(){
+        String mensagemdeErro = "CPF não pode ser vazio";
+        given()
+            .body(usuarioSemDados.toJSONString())
+            .contentType(ContentType.JSON)
+        .when()
+            .post(app.getUrl() + "simulacoes")
+        .then()
+            .statusCode(400)
+            .body(containsString("erros"))
+            .and()
+            .body(containsString(mensagemdeErro));
+
+    }
+
+    @Test
+    @Order(8)
+    public void enviarEdicaodedaSimulacao(){
+        given()
+            .body(usuarioEdicao.toJSONString())
+            .contentType(ContentType.JSON)
+        .when()
+            .put(app.getUrl() + "simulacoes/" + usuario.get("cpf").toString())
+        .then()
+            .statusCode(200)
+            .body(containsString(usuario.get("cpf").toString()));
+    }
+
+    @Test
+    @Order(9)
+    public void apagarPublicacao(){
+        response = httpRequest.get(app.getUrl() + "simulacoes/" + usuario.get("cpf").toString());
+        app.setID(response.getBody().jsonPath().get("id").toString());
+        given()
+        .when()
+            .delete(app.getUrl() + "simulacoes/" + app.getID())
+        .then()
+            .statusCode(200)
+            .body(containsString("OK"));
+
+    }
+
+
 }
